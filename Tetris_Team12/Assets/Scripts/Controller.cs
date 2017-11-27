6e189bed1083ec8da3b41782a9b343a22adcb2d7
John-OMeara <C00206417@itcarlow.ie>
2017-11-27 12:25:58 +0000

    Game working, extras needed

---
+++ Tetris_Team12/Assets/Scripts/Controller.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+    public GameObject[] groups;
+
+	// Use this for initialization
+	void Start () {
+        SpawnNext();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SpawnNext()
+    {
+        int i = Random.Range(0, groups.Length);
+
+        Instantiate(groups[i],
+            transform.position,
+            Quaternion.identity);
+    }
+}
---
+++ Tetris_Team12/Assets/Scripts/Group.cs
@@ -0,0 +1,161 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Group : MonoBehaviour {
+    
+    // Use this for initialization
+    void Start()
+    {
+
+        // Default position not valid? Then it's game over
+        if (!IsValidGridPos())
+        {
+            Debug.Log("GAME OVER");
+            Destroy(gameObject);
+        }
+
+        //StartCoroutine(Fall());
+        InvokeRepeating("Fall", 0, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move Left
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            // Modify position
+            transform.position += new Vector3(-1, 0, 0);
+
+            // See if valid
+            if (IsValidGridPos())
+                // It's valid. Update grid.
+                UpdateGrid();
+            else
+                // It's not valid. revert.
+                transform.position += new Vector3(1, 0, 0);
+        }
+
+        // Move Right
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            // Modify position
+            transform.position += new Vector3(1, 0, 0);
+
+            // See if valid
+            if (IsValidGridPos())
+                // It's valid. Update grid.
+                UpdateGrid();
+            else
+                // It's not valid. revert.
+                transform.position += new Vector3(-1, 0, 0);
+        }
+
+        // Rotate
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.Rotate(0, 0, -90);
+
+            // See if valid
+            if (IsValidGridPos())
+                // It's valid. Update grid.
+                UpdateGrid();
+            else
+                // It's not valid. revert.
+                transform.Rotate(0, 0, 90);
+        }
+
+        // Move Downwards and Fall
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            // Modify position
+            transform.position += new Vector3(0, -1, 0);
+
+            // See if valid
+            if (IsValidGridPos())
+            {
+                // It's valid. Update grid.
+                UpdateGrid();
+            }
+            else
+            {
+                // It's not valid. revert.
+                transform.position += new Vector3(0, 1, 0);
+
+                // Clear filled horizontal lines
+                Grid.DeleteFullRows();
+
+                // Spawn next Group
+                FindObjectOfType<Controller>().SpawnNext();
+
+                // Disable script
+                enabled = false;
+            }
+        }
+    }
+
+    void Fall()
+    {
+        // Modify position
+        transform.position += new Vector3(0, -1, 0);
+
+        // See if valid
+        if (IsValidGridPos())
+        {
+            // It's valid. Update grid.
+            UpdateGrid();
+        }
+        else
+        {
+            // It's not valid. revert.
+            transform.position += new Vector3(0, 1, 0);
+
+            // Clear filled horizontal lines
+            Grid.DeleteFullRows();
+
+            // Spawn next Group
+            FindObjectOfType<Controller>().SpawnNext();
+
+            // Disable script
+            enabled = false;
+        }
+        
+    }
+
+    bool IsValidGridPos()
+    {
+        foreach (Transform child in transform)
+        {
+            Vector2 v = Grid.RoundVec2(child.position);
+            Debug.Log(v);
+        
+            // Not inside Border?
+            if (!Grid.InsideBorder(v))
+                return false;
+        
+            // Block in grid cell (and not part of same group)?
+            if (Grid.grid[(int)v.x, (int)v.y] != null &&
+                Grid.grid[(int)v.x, (int)v.y].parent != transform)
+                return false;
+        }
+        return true;
+    }
+
+    void UpdateGrid()
+    {
+        // Remove old children from grid
+        for (int y = 0; y < Grid.h; ++y)
+            for (int x = 0; x < Grid.w; ++x)
+                if (Grid.grid[x, y] != null)
+                    if (Grid.grid[x, y].parent == transform)
+                        Grid.grid[x, y] = null;
+
+        // Add new children to grid
+        foreach (Transform child in transform)
+        {
+            Vector2 v = Grid.RoundVec2(child.position);
+            Grid.grid[(int)v.x, (int)v.y] = child;
+        }
+    }
+}
